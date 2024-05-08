7b2e33efb6c5acdce44d0f03957d32878bc52d75
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-08 14:59:37 +0200

    finish snacka

--- snack-a/main.js
+++ snack-a/main.js
@@ -1,16 +1,16 @@
 
-function countElements (array){
+function countElements (array){//CREO UNÀFUNZIONE PER CONTARE GLI ELEMENTI ALL'INTERNO DELL'ARRAY
 
-    for ( i=0 ;  array[i]!== undefined; i++){
+    for ( i=0 ;  array[i]!== undefined; i++){//CREO UN CICLO FOR CHE INCREMENTI i DI 1 FINCHE L'ELEMENTO DELL'ARRAY IN POSIZIONE I NON SARA UGUALE À UNDEFINED
     
-        if (array[i] !== undefined) console.log(array[i]);     
+        if (array[i] !== undefined) console.log(array[i]); //STAMPO IL VALORE DELL'ARRAY [i]
     }
-    console.log("l'array è lungo ")
-    return i;
+
+    return i;//RITORNO IL VALORE DELL'ARRAY CHE HO DOPO CHÈIL CICLO HA FINITO
 }
 
 
-const mario = ['mela',  'bamana', 'pera' , 'anguria', 'anguriaf', 'anguriae']
+const mario = ['mela',  'banana', 'pera' , 'anguria', 'kiwi', 'pesca']//CRAO UNA ARRAY CON UN TOT DI ELEMENTI DENTRO
 
 
-console.log(countElements(mario));
+console.log(countElements(mario));//STAMPO IN CONSOLE IL NUMERO DEGLI ELEMENTI CHE CI SONO SLL'UNTERNO DELL'ARRAY
